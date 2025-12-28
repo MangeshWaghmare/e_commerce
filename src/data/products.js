3d343fa428e827285c8cfff85622a9b9002ef121
Mangesh W. <mangeshwaghmare112@gmail.com>
2025-12-28 15:17:32 +0530

    records updated

--- src/data/products.js
+++ src/data/products.js
@@ -2,6 +2,8 @@ const products = [
   { id: 1, name: "Laptop", price: 500 },
   { id: 2, name: "Headphones", price: 100 },
   { id: 3, name: "Mobile Phone", price: 300 },
+  { id: 4, name: "Tablet", price: 200 },
+  { id: 3, name: "Charger", price: 50 },
 ];
 
 export default products;
