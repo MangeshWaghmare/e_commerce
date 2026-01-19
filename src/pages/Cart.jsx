45806b750f612a717bf7c3d0d8a8d5ee25f5e295
Mangesh W. <mangeshwaghmare112@gmail.com>
2026-01-19 15:45:55 +0530

    code updated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,7 @@
 function Cart() {
   return (
     <div style={{ padding: "20px" }}>
-      <h2>Cart Page</h2>
+      <h2>Cart Page !!</h2>
       <p>Cart functionality coming next!</p>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 function Login() {
   return (
     <div style={{ padding: "20px" }}>
-      <h2>Login Page</h2>
+      <h2>Login Page !! </h2>
       <p>Login functionality coming next!</p>
     </div>
   );
